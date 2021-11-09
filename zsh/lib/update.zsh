763ea4ff05dbedf67fee783c01061db84d3c8525
Alexey Orlenko <alex@aqrln.net>
2021-11-10 00:22:45 +0200

    Add headers in update command output

--- zsh/lib/update.zsh
+++ zsh/lib/update.zsh
@@ -1,4 +1,4 @@
-function update {
+update () {
     update_apt
     update_brew
     update_nix
@@ -8,51 +8,66 @@ function update {
     update_npm
 }
 
-function update_apt {
+__update_show_header () {
+    printf "\033[1;36m\n$1\n\n\033[0m"
+}
+
+update_apt () {
     # Check for apt-get instead of apt because macOS has unrelated /usr/bin/apt binary
     if type apt-get &>/dev/null; then
+        __update_show_header 'Updating APT packages'
+
         apt update
         apt upgrade -y
     fi
 }
 
-function update_brew {
+update_brew () {
     if type brew &>/dev/null; then
+        __update_show_header "Updating Homebrew packages"
+
         brew update
         brew upgrade
     fi
 }
 
-function update_nix {
+update_nix () {
     if type nix-env &>/dev/null; then
+        __update_show_header "Updating Nix packages"
+
         nix-channel --update nixpkgs
         nix-env -u '*'
     fi
 }
 
-function update_rust {
+update_rust () {
     if type rustup &>/dev/null; then
+        __update_show_header "Updating Rust"
         rustup update
-        rustup upgrade
     fi
 }
 
-function update_cargo {
+update_cargo () {
     if type cargo-install-update &>/dev/null; then
+        __update_show_header "Updating Cargo crates"
         cargo install-update -a
     else
+        __update_show_header "Installing cargo-update"
         cargo install cargo-update && update_cargo
     fi
 }
 
-function update_node {
+update_node () {
     if type nvm &>/dev/null; then
+        __update_show_header "Updating Node.js"
         nvm install node --reinstall-packages-from=node
     fi
 }
 
-function update_npm {
+update_npm () {
     if type npm &>/dev/null; then
+        __update_show_header "Updating global npm packages"
+
         npm install -g npm@latest
         npm list -gp --depth 0 | \
             awk -F / '{ if (NR > 1 && $NF != "npm") print $NF "@latest" }' | \
