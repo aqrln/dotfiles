5da5169858634ffd16c7eac8c9c7f92bb21c3bc4
Alexey Orlenko <alex@aqrln.net>
2021-12-20 21:57:58 +0200

    Support both direct and indirect OpenGL

--- zsh/lib/wsl.zsh
+++ zsh/lib/wsl.zsh
@@ -1,7 +1,10 @@
 if [[ -n "$WSL_DISTRO_NAME" ]]; then
     x11-forward () {
         export DISPLAY=$(ip route | awk '/^default/{print $3; exit}'):0
-        export LIBGL_ALWAYS_INDIRECT=1
+        export LIBGL_ALWAYS_INDIRECT=$1
         printf "Start X11 using \033[1;36mvcxsrv -ac\033[0m in Windows.\n"
     }
+
+    alias xdirect="x11-forward 0"
+    alias xindirect="x11-forward 1"
 fi
