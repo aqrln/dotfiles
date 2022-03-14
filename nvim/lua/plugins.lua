1a42bb7dd72c605f1f1ec26bfe3e60a78f221d1a
Alexey Orlenko <alex@aqrln.net>
2022-03-15 00:11:14 +0100

    nvim: fix formatting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -53,7 +53,8 @@ return require('packer').startup(function()
     }
   }
 
-  use { 'TimUntersberger/neogit',
+  use {
+    'TimUntersberger/neogit',
     requires = {
       'nvim-lua/plenary.nvim',
       'sindrets/diffview.nvim',
