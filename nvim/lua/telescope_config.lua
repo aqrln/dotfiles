4150969f4cceb997190ba40738ac9cdf34bb2866
Alexey Orlenko <alex@aqrln.net>
2022-03-14 21:44:53 +0100

    nvim: add more telescope keybindings

--- nvim/lua/telescope_config.lua
+++ nvim/lua/telescope_config.lua
@@ -4,20 +4,35 @@ require('telescope').setup {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = true,  -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- "ignore_case" or "respect_case"
+      case_mode = 'smart_case',        -- 'ignore_case' or 'respect_case'
     }
   }
 }
 
 require('telescope').load_extension('fzf')
 
-local prefix = "<space>f"
+local prefix = '<space>'
 
 local keybindings = {
-  f = "find_files()",
-  g = "live_grep()",
-  b = "buffers()",
-  h = "help_tags()",
+  [' '] = 'builtin()',
+
+  fr = 'resume()',
+  ff = 'find_files()',
+  fF = 'git_files()',
+  fg = 'live_grep()',
+  fG = 'grep_string()',
+  fb = 'buffers()',
+  fh = 'help_tags()',
+
+  lr = 'lsp_references()',
+  ls = 'lsp_document_symbols()',
+  lS = 'lsp_dynamic_workspace_symbols()',
+  la = 'lsp_code_actions()',
+  lA = 'lsp_range_code_actions()',
+  lD = 'diagnostics()',
+  li = 'lsp_implementations()',
+  ld = 'lsp_definitions()',
+  lt = 'lsp_type_definitions()',
 }
 
 for key, method in pairs(keybindings) do
