e026815dbf29233edee1eeb69335882f91c3cb2e
Alexey Orlenko <alex@aqrln.net>
2022-03-15 17:06:21 +0100

    nvim: add treesitter incremental selection

--- nvim/lua/treesitter_config.lua
+++ nvim/lua/treesitter_config.lua
@@ -1,15 +1,10 @@
 require('nvim-treesitter.configs').setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = "maintained",
-
-  -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- List of parsers to ignore installing
   ignore_install = {},
 
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
 
     -- list of language that will be disabled
@@ -24,6 +19,16 @@ require('nvim-treesitter.configs').setup {
 
   indent = {
     enable = true,
-  }
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
 
