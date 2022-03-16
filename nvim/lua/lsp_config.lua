fa042fa68de26514d2804cea755d8b42f01012b7
Alexey Orlenko <alex@aqrln.net>
2022-03-16 16:32:44 +0100

    nvim: spaces around =

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -2,7 +2,7 @@
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
