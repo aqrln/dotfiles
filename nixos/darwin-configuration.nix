8cef5101dc6f94b0d5eedf00c798402cbfe495d4
Alexey Orlenko <alex@aqrln.net>
2023-01-15 23:10:39 +0100

    nix: enable bash

--- nixos/darwin-configuration.nix
+++ nixos/darwin-configuration.nix
@@ -21,6 +21,7 @@
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
+  programs.bash.enable = true;
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
