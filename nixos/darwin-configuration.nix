146577b7526dfbf915063971269eee817750c9e1
Alexey Orlenko <alex@aqrln.net>
2023-02-16 22:30:05 +0100

    nixos: add commented-out sandbox option for reference

--- nixos/darwin-configuration.nix
+++ nixos/darwin-configuration.nix
@@ -17,8 +17,10 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # nix.settings.sandbox = true;
 
   nix.gc = {
     automatic = true;
