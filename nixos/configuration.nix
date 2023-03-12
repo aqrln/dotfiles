320bab6545f0bd053f64a1c47382265675a298de
Alexey Orlenko <alex@aqrln.net>
2023-03-12 22:55:59 +0100

    nixos: enable nix-direnv

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,6 +139,8 @@
   home-manager.users.aqrln = {
     home.stateVersion = "22.11";
     programs.bash.enable = true;
+    programs.direnv.enable = true;
+    programs.direnv.nix-direnv.enable = true;
   };
 
   # Allow unfree packages
