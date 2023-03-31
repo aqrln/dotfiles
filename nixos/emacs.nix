c28740e656cc06b8227c9dd75596ed891a820b9f
Alexey Orlenko <alex@aqrln.net>
2023-03-31 12:51:06 +0200

    nixos: use stable emacs for now

--- nixos/emacs.nix
+++ nixos/emacs.nix
@@ -6,15 +6,16 @@ let
       config = ../emacs/init.el;
       alwaysEnsure = true;
 
-      package = emacsPgtk.overrideAttrs (prev: {
-        patches = (prev.patches or [ ]) ++ [
-          (fetchpatch {
-            url =
-              "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-            sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-          })
-        ];
-      });
+      package = emacs;
+      # package = emacsPgtk.overrideAttrs (prev: {
+      #   patches = (prev.patches or [ ]) ++ [
+      #     (fetchpatch {
+      #       url =
+      #         "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+      #       sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+      #     })
+      #   ];
+      # });
     };
 
 in {
