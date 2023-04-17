205f9b0750dbcf67c29ee08394d3fbcaeee43578
Alexey Orlenko <alex@aqrln.net>
2023-04-17 19:39:21 +0200

    emacs: add git fringes

--- emacs/init.el
+++ emacs/init.el
@@ -289,6 +289,17 @@
   :init
   (setq auth-sources '("~/.authinfo")))
 
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
+  (setq git-gutter-fr:side 'left-fringe))
+
 (use-package goto-chg)
 
 (use-package evil
