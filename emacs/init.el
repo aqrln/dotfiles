1ab20149c224acc3f4830e04714b07f526776f5c
Alexey Orlenko <alex@aqrln.net>
2023-04-17 19:55:06 +0200

    emacs: improve git gutter color

--- emacs/init.el
+++ emacs/init.el
@@ -298,7 +298,8 @@
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom)
-  (setq git-gutter-fr:side 'left-fringe))
+  (setq git-gutter-fr:side 'left-fringe)
+  (set-face-foreground 'git-gutter-fr:modified "orange"))
 
 (use-package goto-chg)
 
