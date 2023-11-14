eaa1d8b596bbcb0af6004bfe37ee58835e84f119
Alexey Orlenko <alex@aqrln.net>
2023-11-14 15:51:46 +0100

    nvim: fix autopair completion for generics

--- nvim/lua/autopairs_config.lua
+++ nvim/lua/autopairs_config.lua
@@ -14,7 +14,7 @@ cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 npairs.add_rules {
   Rule('<', '>', {'rust', 'typescript'})
     :with_pair(conds.not_before_text(' '))
-    :with_move(conds.after_text('>'))
+    :with_move(conds.move_right())
     -- :with_pair(ts_conds.is_not_ts_node({'binary_expression'}))
     -- :with_pair(ts_conds.is_ts_node({
     --   'type_arguments',
