b6526bf73ecc36747faeb18932986b1ee74c3a39
Alexey Orlenko <alex@aqrln.net>
2023-12-11 22:59:51 +0100

    hosts/asahi: add a bunch of packages

--- nixos/hosts/asahi/home.nix
+++ nixos/hosts/asahi/home.nix
@@ -17,16 +17,29 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [
+    fd
+    fzf
+    nil
+    nodejs_latest
+    nodejs_latest.pkgs."@prisma/language-server"
+    nodejs_latest.pkgs.dockerfile-language-server-nodejs
+    nodejs_latest.pkgs.pnpm
+    nodejs_latest.pkgs.prettier
+    nodejs_latest.pkgs.typescript-language-server
+    nodejs_latest.pkgs.vscode-langservers-extracted
+    nodejs_latest.pkgs.yarn
+    rustup
+    tmux
+    vscode-extensions.vadimcn.vscode-lldb.adapter
+    yaml-language-server
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (pkgs.nerdfonts.override { fonts = [ "Iosevka" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
