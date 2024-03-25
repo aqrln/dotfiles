2d9f7edd44229e0ca3caa47359b5f3150f9000f0
Alexey Orlenko <alex@aqrln.net>
2024-03-25 11:27:52 +0100

    hosts/minibloom: update configuration due to upstream changes

--- nixos/hosts/minibloom/configuration.nix
+++ nixos/hosts/minibloom/configuration.nix
@@ -84,9 +84,9 @@
   # services.xserver.displayManager.gdm.wayland = false;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
