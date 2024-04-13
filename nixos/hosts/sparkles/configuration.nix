db674cb68ce410a2878a3e97c319922cf18738a2
Alexey Orlenko <alex@aqrln.net>
2024-04-14 01:24:01 +0200

    hosts/sparkles: try out kde

--- nixos/hosts/sparkles/configuration.nix
+++ nixos/hosts/sparkles/configuration.nix
@@ -67,8 +67,10 @@
   services.xserver.libinput.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
