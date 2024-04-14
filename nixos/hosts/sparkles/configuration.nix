678c099ef94a25ef54cbd6d0100e808a8cbc3d64
Alexey Orlenko <alex@aqrln.net>
2024-04-14 04:40:08 +0200

    hosts/sparkles: plasma5 -> plasma6

--- nixos/hosts/sparkles/configuration.nix
+++ nixos/hosts/sparkles/configuration.nix
@@ -70,7 +70,7 @@
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
