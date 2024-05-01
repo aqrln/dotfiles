143ab447ab1358d138fcf4560bd0803ced1c23b3
Alexey Orlenko <alex@aqrln.net>
2024-05-01 20:08:13 +0200

    hosts/sparkles: access stlink without root

--- nixos/embedded-dev.nix
+++ nixos/embedded-dev.nix
@@ -6,4 +6,12 @@
     stlink-gui
     usbutils
   ];
+
+  # Read/write access to ST-Link V2 without root.
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", TAG+="uaccess"
+  '';
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="0666"
+  # '';
 }
