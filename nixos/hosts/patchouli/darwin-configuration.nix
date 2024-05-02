11f8171eb1efd7c64ac89403baadf7ceca2999ed
Alexey Orlenko <alex@aqrln.net>
2024-05-02 23:14:52 +0200

    hosts/patchouli: add `act` program

--- nixos/hosts/patchouli/darwin-configuration.nix
+++ nixos/hosts/patchouli/darwin-configuration.nix
@@ -11,6 +11,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    act
     bun
     deno
     fd
