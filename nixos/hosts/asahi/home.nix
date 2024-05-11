b691a1bf843fe487a971ef70145e2f86c3de9c85
Alexey Orlenko <alex@aqrln.net>
2024-05-11 21:26:44 +0200

    hosts/asahi: add embedded dev tools

--- nixos/hosts/asahi/home.nix
+++ nixos/hosts/asahi/home.nix
@@ -34,6 +34,9 @@
     vscode-extensions.vadimcn.vscode-lldb.adapter
     yaml-language-server
 
+    cargo-espflash
+    probe-rs
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
