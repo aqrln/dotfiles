971df721b909825288f8ec45ac153b10b3adf5c4
Alexey Orlenko <alex@aqrln.net>
2024-05-12 20:16:57 +0200

    hosts/asahi: try out foot

--- nixos/hosts/asahi/home.nix
+++ nixos/hosts/asahi/home.nix
@@ -100,6 +100,27 @@
 
       # uncomment this if there are any issues with rootless docker socket not being found
       # export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+      osc7_cwd() {
+        local strlen=''${#PWD}
+        local encoded=""
+        local pos c o
+        for (( pos=0; pos<strlen; pos++ )); do
+            c=''${PWD:$pos:1}
+            case "$c" in
+                [-/:_.!\'\(\)~[:alnum:]] ) o="''${c}" ;;
+                * ) printf -v o '%%%02X' "'$c" ;;
+            esac
+            encoded+="''${o}"
+        done
+        printf '\e]7;file://%s%s\e\\' "''${HOSTNAME}" "''${encoded}"
+    }
+    PROMPT_COMMAND=''${PROMPT_COMMAND:+$PROMPT_COMMAND; }osc7_cwd
+
+    prompt_marker() {
+        printf '\e]133;A\e\\'
+    }
+    PROMPT_COMMAND=''${PROMPT_COMMAND:+$PROMPT_COMMAND; }prompt_marker
     '';
   };
 
@@ -157,5 +178,19 @@
         };
       };
     };
+  };
+
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "Inconsolata Nerd Font:size=10";
+        # dpi-aware = "yes";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+
   };
 }
