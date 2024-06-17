c98937a9c8dac5300fb5aba6b2612777d59a692e
Alexey Orlenko <alex@aqrln.net>
2024-06-17 13:21:26 +0200

    nvim: enable inlay hints in ts

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -240,6 +240,14 @@ lspconfig.vtsls.setup {
 
   settings = {
     typescript = {
+      inlayHints = {
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        variableTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        enumMemberValues = { enabled = true },
+      }
     },
   },
 }
