61f106e3989b59067629b361427e76eafdac59aa
Alexey Orlenko <alex@aqrln.net>
2024-10-24 09:51:17 +0200

    hosts/minibloom: remove deprecated sound option

--- nixos/hosts/minibloom/configuration.nix
+++ nixos/hosts/minibloom/configuration.nix
@@ -90,7 +90,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
