0b2cc12c939cfa51d9dd825802db0b42ac9672ea
Alexey Orlenko <alex@aqrln.net>
2024-11-16 15:29:32 +0100

    hosts/iris: vnc setup

--- nixos/hosts/iris/configuration.nix
+++ nixos/hosts/iris/configuration.nix
@@ -12,6 +12,20 @@ let
     '';
   };
 
+  xstartup = pkgs.writeShellApplication {
+    name = "xstartup";
+    text = ''
+      ${pkgs.plasma-workspace}/bin/startplasma-x11
+    '';
+  };
+
+  vnc = pkgs.writeShellApplication {
+    name = "vnc";
+    text = ''
+      vncserver -vgl -xstartup ${xstartup}/bin/xstartup -geometry 3024x1890
+    '';
+  };
+
 in
 
 with lib;
@@ -79,6 +93,7 @@ with lib;
       yaml-language-server
 
       x-www-browser
+      vnc
     ];
   };
 
@@ -132,6 +147,7 @@ with lib;
     ripgrep
     tailscale
     tmux
+    turbovnc
     wget
     zsh
     xorg.xauth
@@ -220,7 +236,8 @@ with lib;
 
   programs.ssh.setXAuthLocation = lib.mkForce true;
 
-  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  programs.turbovnc.ensureHeadlessSoftwareOpenGL = true;
 
   home-manager.users.aqrln.home.file.".gnupg/gpg-agent.conf".text =
     let
