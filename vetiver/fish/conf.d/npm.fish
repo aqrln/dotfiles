6cebc9ade4bacbd00e126bdf6b75ab7639353bfc
Oleksii Orlenko <alex@aqrln.net>
2026-02-01 15:32:47 +0100

    vetiver: clean slate

---
+++ vetiver/fish/conf.d/npm.fish
@@ -0,0 +1,2 @@
+set -x NPM_PACKAGES ~/.local/share/npm-packages
+fish_add_path --path "$NPM_PACKAGES/bin"
---
+++ vetiver/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias hx="helix"
+
+set -gx VISUAL helix
+set -gx EDITOR helix
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
