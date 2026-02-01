a96b2e0ff557fa921ac7ddd6c6c3a9c5cc95a189
Oleksii Orlenko <alex@aqrln.net>
2026-02-01 21:26:29 +0100

    vetiver: move to pure arch

--- vetiver/fish/config.fish
+++ vetiver/fish/config.fish
@@ -1,12 +1,8 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
 alias hx="helix"
 
 set -gx VISUAL helix
 set -gx EDITOR helix
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
